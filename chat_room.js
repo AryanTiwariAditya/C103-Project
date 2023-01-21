92c9bce894da317a1e06e61b160b77021a88afc6
ATMaster Creator <97293505+AryanTiwariAditya@users.noreply.github.com>
2023-01-21 19:45:15 +0530

    Update chat_room.js

--- chat_room.js
+++ chat_room.js
@@ -15,19 +15,19 @@ user_name = localStorage.getItem("user_name");
 document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
 
 function addRoom()
-{
+    {
       room_name = document.getElementById("room_name").value;
 
       firebase.database().ref("/").child(room_name).update({
             purpose : "adding room name"
-});
+      });
 
       localStorage.setItem("room_name", room_name);
       window.location = "chat_page.html";
-}
+    }
 
-function getData()
-{
+    function getData()
+    {
         firebase.database().ref("/").on('value', function (snapshot) {document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
         
           Room_names = childKey;
@@ -36,22 +36,22 @@ function getData()
           row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
           document.getElementById("output").innerHTML += row;
           //End Code
-});});}
-getData();
+    });});}
+    getData();
 
-function redirectToRoomName(name)
-{
+    function redirectToRoomName(name)
+    {
       console.log(name);
       localStorage.setItem("room_name", name);
       window.location = "chat_page.html";
-}
+    }
 
-function logout()
-{
+    function logout()
+    {
       localStorage.removeItem("user_name");
       localStorage.removeItem("room_name");
       window.location = "index.html";
-}
+    }
 
 
     
